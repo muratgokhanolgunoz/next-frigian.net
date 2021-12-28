dd11b55fde8cefcfa59e12a0a335ae6b2bbac5fa
Murat Gökhan Olgunöz <gokhanolgunoz@gmail.com>
2021-12-28 10:51:29 +0300

    # The homepage video section has been edited

--- src/components/frontend/pages/home/Index.jsx
+++ src/components/frontend/pages/home/Index.jsx
@@ -1,18 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Banner from "./Banner";
 import Video from "./Video";
 import Why from "./Why";
 import Widget from "./Widget";
+import { useRouter } from "next/router";
+import { useTranslation } from "next-i18next";
+import { Container, Row, Col } from "react-bootstrap";
+import Modal from "../../../../../_tools/components/Modal";
+import videosJson from "../../../../../_utils/videos.json";
 
 const Index = () => {
-    return (
-        <div id="home">
-            <Banner />
-            <Widget />
-            <Why />
-            <Video />
-        </div>
-    );
+  const [activeVideo, setActiveVideo] = useState(undefined);
+  const [modalState, setModalState] = useState(false);
+  const router = useRouter();
+  const { t } = useTranslation("common");
+
+  console.log(videosJson[1].path);
+
+  useEffect(() => {
+    if (
+      router.query.video !== undefined &&
+      router.query.video <= videosJson.length
+    ) {
+      setActiveVideo(router.query.video);
+      setModalState(true);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <div id="home">
+      <Banner />
+      <Widget />
+      <Why />
+      <Container fluid style={{ padding: "0" }}>
+        <Video
+          video={videosJson[router.locale === "tr" ? 0 : 1].path}
+          background={`./assets/img/frigian-dashboard.jpg`}
+          title={t("home.video.VIDEO_TEXT")}
+        />
+      </Container>
+      {modalState && (
+        <Modal selector="#__modal" toogle={setModalState}>
+          <embed
+            src={activeVideo !== undefined && videosJson[activeVideo].path}
+          ></embed>
+        </Modal>
+      )}
+    </div>
+  );
 };
 
 export default Index;
--- src/components/frontend/pages/home/Video.jsx
+++ src/components/frontend/pages/home/Video.jsx
@@ -1,47 +1,49 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
 import { Container, Row } from "react-bootstrap";
-import { useTranslation } from "next-i18next";
 import { ImPlay3 } from "react-icons/im";
 import style from "../../../../../styles/Video.module.scss";
 import Modal from "../../../../../_tools/components/Modal";
+import videosJson from "../../../../../_utils/videos.json";
 import { useRouter } from "next/router";
 
-const Video = (_) => {
-    const { t } = useTranslation("common");
-    const [modalState, setModalState] = useState(false);
-    const router = useRouter();
+const Video = ({ video, background, title }) => {
+  const [modalState, setModalState] = useState(false);
 
-    const styles = {
-        background: "url(./assets/img/frigian-dashboard.jpg)",
-        backgroundAttachment: "fixed",
-        backgroundSize: "cover",
-        backgroundRepeat: "no-repeat",
-        backgroundPosition: "center",
-    };
+  const styles = {
+    background: "url(" + background + ")",
+    backgroundAttachment: "fixed",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "right top",
+  };
 
-    return (
-        <div id="video" className={style.video} style={styles}>
-            <Container>
-                <Row className={style.videoInner}>
-                    <span>
-                        <a onClick={() => setModalState(true)}>
-                            <ImPlay3 className={style.videoIcon} />
-                        </a>
-                    </span>
-                    <br />
-                    <h1>{t("home.video.VIDEO_TEXT")}</h1>
-                </Row>
-            </Container>
-            {modalState && (
-                <Modal selector="#__modal" toogle={setModalState}>
-                    <embed
-                        src={"./assets/videos/intro_" + router.locale + ".mp4"}
-                    ></embed>
-                </Modal>
-            )}
-        </div>
-    );
+  return (
+    <div id="video" className={style.video} style={styles}>
+      <Container>
+        <Row className={style.videoInner}>
+          <span>
+            <a onClick={() => setModalState(true)}>
+              <ImPlay3 className={style.videoIcon} />
+            </a>
+          </span>
+          <br />
+          <h1>{title}</h1>
+        </Row>
+      </Container>
+      {modalState && (
+        <Modal selector="#__modal" toogle={setModalState}>
+          <embed src={video}></embed>
+        </Modal>
+      )}
+    </div>
+  );
+};
+
+Video.propTypes = {
+  video: PropTypes.number.isRequired,
+  background: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
 };
 
 export default Video;
